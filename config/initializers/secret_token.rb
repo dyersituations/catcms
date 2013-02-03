3ea9dac1d6df9085f98fcc2faa3473e9cdb4c883
Casey Dyer <dyersituations@gmail.com>
2013-02-03 14:44:15 -0800

    -first commit -blank app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DynamicArtsite::Application.config.secret_token = 'bd11ffa1b02ef253fa72a86971031a4776352fcea21dfe8c3998f0006127e28fe4633b5f739b5fb7e4a11e0412ede5680ba32ea1ebb9dce70ada8f48e6c7546a'
