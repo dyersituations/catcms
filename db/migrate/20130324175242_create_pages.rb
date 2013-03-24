aeb069d8a67a5f7d1b165e01938546e599b73b4c
Casey Dyer <dyersituations@gmail.com>
2013-03-24 11:41:18 -0800

    DB can now be properly reset. -The migration files were missing (I deleted before without thinking). -Included updated automatically created test files.

---
+++ db/migrate/20130324175242_create_pages.rb
@@ -0,0 +1,12 @@
+class CreatePages < ActiveRecord::Migration
+  def change
+    create_table :pages do |t|
+      t.integer :page_type
+      t.string :path
+      t.string :title
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
