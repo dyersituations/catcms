a588dfc767f0e99cce4ebd287698d7025cec51a4
Casey Dyer <dyersituations@gmail.com>
2013-05-26 19:51:52 -0700

    Minor changes for subdomain -Gallery images now open -Root redirect works -Clicking title now returns to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 DynamicArtsite::Application.routes.draw do
-  resources :posts
+  # root
+  root :to => 'pages#show', :defaults => {:path => 'home'}
 
   # Admin user management routes
   match 'admin' => 'admin#admin'
   match 'login' => 'admin#login'
   match 'logout' => 'admin#logout'
+
+  # posts
+  resources :posts
 
   # Pages routes
   match 'pages/new' => 'pages#new'
@@ -58,10 +62,6 @@ DynamicArtsite::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => redirect('/home')
 
   # See how all your routes lay out with "rake routes"
 
