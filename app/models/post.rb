4afb9369df5caf273a29f26516731258586740f3
Casey Dyer <dyersituations@gmail.com>
2013-07-30 20:52:57 -0700

    Tag is now category -Category more clear name for usage -Gallery categories weren't in alphabetical order because of case

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :image, :title, :page_path, :tag
+  attr_accessible :content, :image, :title, :page_path, :category
   
   mount_uploader :image, PostUploader
 end
