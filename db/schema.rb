0064562ac23b51297a6675770b3742b4395500e3
Casey Dyer <dyersituations@gmail.com>
2013-09-14 14:54:47 -0700

    Removed files not needed in repo.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130731033339) do
+ActiveRecord::Schema.define(:version => 20130324175242) do
 
   create_table "pages", :force => true do |t|
     t.integer  "page_type"
@@ -28,9 +28,9 @@ ActiveRecord::Schema.define(:version => 20130731033339) do
     t.string   "title"
     t.string   "image"
     t.text     "content"
+    t.string   "category"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.string   "category"
   end
 
 end
