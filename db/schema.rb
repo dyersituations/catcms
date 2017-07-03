abdb0b19dcdfc3cc6703fb0c55a31a09c010e1b6
Casey Dyer <dyersituations@gmail.com>
2017-07-02 17:05:47 -0700

    Including changes from db:migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -14,22 +14,22 @@ ActiveRecord::Schema.define(version: 20160221203927) do
 
   create_table "pages", force: :cascade do |t|
     t.integer  "page_type"
-    t.string   "path"
-    t.string   "banner"
-    t.string   "title"
+    t.string   "path",       limit: 255
+    t.string   "banner",     limit: 255
+    t.string   "title",      limit: 255
     t.text     "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string   "page_path"
-    t.string   "title"
-    t.string   "image"
+    t.string   "page_path",  limit: 255
+    t.string   "title",      limit: 255
+    t.string   "image",      limit: 255
     t.text     "content"
-    t.string   "category"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.string   "category",   limit: 255
   end
 
   create_table "settings", force: :cascade do |t|
