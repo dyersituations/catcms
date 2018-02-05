00ebc021e5a61429a4e67923703793c3b7762baf
Casey Dyer <dyersituations@gmail.com>
2018-02-04 22:26:37 -0800

    Removed page title, simplified to just use path. Fixed removing pages. Added path to browser page title.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160221203927) do
+ActiveRecord::Schema.define(version: 20180205023900) do
 
   create_table "pages", force: :cascade do |t|
     t.integer  "page_type"
     t.string   "path"
     t.string   "banner"
-    t.string   "title"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
