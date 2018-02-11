4930db74828bbab118d1ea8c28dfa25a37c8354b
Casey Dyer <dyersituations@gmail.com>
2018-02-10 22:13:15 -0800

    Added edit posts page as separate editing experience with footer button. Updated button styles. Added form cancel buttons.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,12 @@ class ApplicationController < ActionController::Base
     @pages = Page.all
   end
 
+  protected
+
+  def post_cats
+    Post.pluck(:category).uniq{ |c| c.downcase }.sort
+  end
+
   private
 
   def pages_with_posts
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
+  # Page display routes.
   root :to => 'pages#show'
+  # Admin routes.
   get 'login' => 'admin#login'
   post 'login_admin' => 'admin#login_admin'
   get 'admin' => 'admin#admin'
   post 'admin/save' => 'admin#save'
   get 'logout' => 'admin#logout'
+  # Default post and page routes.
   resources :posts
   resources :pages
+  # Post display and edit routes.
+  get 'edit_posts' => 'pages#edit_posts'
   get ':path' => 'pages#show', :as => 'page_view'
-  get ':path/edit' => 'pages#edit'
-  get 'gallery/:category' => 'pages#show', :defaults => {:path => 'gallery'}
+  get 'gallery/:category' => 'pages#show', :defaults => { :path => 'gallery' }
 end
