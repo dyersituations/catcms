03df5aa09b1559e1f977a937116acda290a07671
Casey Dyer <dyersituations@gmail.com>
2019-05-18 18:18:29 -0700

    Fixed issue where you couldn't get to the admin settings if the admin password was blank in the DB (only sent the user to the admin settings before if it was missing from DB).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   def authorize
     if !Settings.instance.admin_pass && params[:controller] != 'admin'
       redirect_to admin_path
-    elsif !session[:admin] && Settings.instance.admin_pass && params[:action] != 'login'
+    elsif !session[:admin] && Settings.instance.admin_pass && !Settings.instance.admin_pass.empty? && params[:action] != 'login'
       session[:login_redirect] = request.original_url
       redirect_to login_path
     end
