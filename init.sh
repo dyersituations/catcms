2fc183686f27156c4cb193c66cab7ce56bc34730
Casey Dyer <dyersituations@gmail.com>
2020-10-15 21:59:13 -0700

    Fixed init script. Moved letsencrypt file to catdocker.

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ find ./public -mindepth 1 -regex '^./public/assets\(/.*\)?' -delete
 
 bundle exec rake assets:precompile
 
-rake db:create db:migrate"bundle", "exec", "puma", "-C", "config/puma.rb"
+rake db:create db:migrate
 
 bundle exec puma -C config/puma.rb
